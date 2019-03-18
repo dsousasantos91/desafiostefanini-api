1da1eb8ed397f61fa45d9e7dc93e83083c03e41e
Douglas de Sousa Santos <douglas.sousa@basis.com.br>
2019-03-18 01:19:12 -0300

    Ajuste cammel case classe principal

--- src/test/java/com/desafiostefanini/desafiostefaniniapi/DesafioStefaniniApiApplicationTests.java
+++ src/test/java/com/desafiostefanini/desafiostefaniniapi/DesafioStefaniniApiApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DesafiostefaniniApiApplicationTests {
+public class DesafioStefaniniApiApplicationTests {
 
 	@Test
 	public void contextLoads() {
